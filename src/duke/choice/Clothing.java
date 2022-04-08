536be8c835ac069d8326ff4c05c044b20f26f43d
deegeeooh <bakkerpepijn69@gmail.com>
2022-04-08 19:42:01 +0200

    Update Clothing.java

--- src/duke/choice/Clothing.java
+++ src/duke/choice/Clothing.java
@@ -1,28 +1,28 @@
 package duke.choice;
 
 public class Clothing {
+
     // properties
     private String description;
     private double price;
     private String size = "M";
 
+    // constructor
     public Clothing(String _description, double _price, String _size) {
         description = _description;
-        price = _price;
-        size = _size;
+        price       = _price;
+        size        = _size;
     }
 
-    public String GetClothingAttributesCSV() {
-        return description + "," + price + "," + size;
-    }
-    
-    public double GetPrice(){
+    public double GetPrice() {
         return price;
     }
-    public String GetDescription(){
+
+    public String GetDescription() {
         return description;
     }
-    public String GetSize(){
+
+    public String GetSize() {
         return size;
     }
 
