5a72d26acb35128224c7884cc552e84874db51ac
deegeeooh <bakkerpepijn69@gmail.com>
2022-04-08 19:42:37 +0200

    Update ShopApp.java

--- src/duke/choice/ShopApp.java
+++ src/duke/choice/ShopApp.java
@@ -1,27 +1,32 @@
 package duke.choice;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ShopApp {
     public static void main(String[] args) {
-        
+
         double tax = 0.2;
-        double total;
-        
-        Customer c1 = new Customer("Pinky");
-        Clothing item1 = new Clothing("Blue Jacket", 20.9, "M");
-        Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S");
-            
-        c1.Purchase(item1);
-        c1.Purchase(item2);
-        total = c1.CalculateTotal();
-        
+        //double total;
+        String[] shoppingBasketCSV;
+
+        // instantiate objects
+        Customer customer1 = new Customer("Pinky");
+        Clothing item1 = new Clothing("Blue Jacket", 20.91, "M");
+        Clothing item2 = new Clothing("Orange T-Shirt", 10.52, "S");
+
+        customer1.Purchase(item1, 1, tax); // Jacket
+        customer1.Purchase(item2, 1, tax); // Tshirts
+
         System.out.println("Welcome to the Duke Choice Shop");
-        System.out.println("The name of the new customer is: " + c1.GetName());
-        System.out.println("Purchased clothing:");
-        System.out.print(item1.GetClothingAttributesCSV() + " " + item2.GetClothingAttributesCSV());
+        System.out.println("The name of the new customer is: " + customer1.GetName());
+        System.out.println("Shopping basket content:");
+
+        shoppingBasketCSV = customer1.ShoppingBasketCSV();
+        if (shoppingBasketCSV.length > 0) {
+            for (int i = 0; i < shoppingBasketCSV.length; i++) {
+                System.out.println(shoppingBasketCSV[i]);
+            }
+        }
+
         System.out.println();
-        System.out.println("Total costs: "+ (total += total * tax));
+        System.out.println("Total costs: " + customer1.CalculateTotalexVAT());
     }
 }
