69a3fee480082d86a952e147db64f51ba7256af3
deegeeooh <bakkerpepijn69@gmail.com>
2022-04-08 19:42:27 +0200

    added shoppingbasket

--- src/duke/choice/Customer.java
+++ src/duke/choice/Customer.java
@@ -1,30 +1,45 @@
 package duke.choice;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Customer {
 
-    private static String Name;
-    private static ArrayList<Clothing> purchasedClothing = new ArrayList<Clothing>();
-    private static double Total;
+    private String Name;
+    private ArrayList<ShoppingBasketItem> ShoppingBasket = new ArrayList<ShoppingBasketItem>(); 
+    private double Total;
+    
     
     public Customer(String _name) {
         Name = _name;
     }
-    public String GetName(){
+
+    public String GetName() {
         return Name;
     }
-    
-    public void Purchase(Clothing _clothing) {
-        purchasedClothing.add(_clothing); 
+
+    public void Purchase(Clothing _clothing, int _amount, double _tax) {
+        ShoppingBasket.add(new ShoppingBasketItem(_clothing, _amount, _tax,LocalDate.now()));
     }
-    
-    public double CalculateTotal(){
-        for (Clothing clothing : purchasedClothing) {
-            Total += clothing.GetPrice();
-        }        
+
+    public double CalculateTotalexVAT() {
+        for (ShoppingBasketItem item : ShoppingBasket) {
+            Total = Total + item.GetTotal(item);    
+        }
         return Total;
     }
     
+    public String[] ShoppingBasketCSV(){
+        
+        String[] basketCSV = new String[ShoppingBasket.size()];
+        int i = 0;
+        for (ShoppingBasketItem item : ShoppingBasket) {
+            basketCSV[i]=item.GetBasketItemAsCSV(item);
+            i++;
+        }
+        return basketCSV;
+        
+    }
     
-}   
+   
+}
